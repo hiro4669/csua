f94491ab5bd0d05af7ae40dc244af6a8801b0d3b
Hiroaki Fukuda <hiroaki@java2.jp>
2018-10-01 09:17:12 +0900

    add memory module

---
+++ comp/memtest.c
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   memtest.c
+ * Author: hiroaki
+ *
+ * Created on October 1, 2018, 7:58 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../memory/MEM.h"
+
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+   
+    char *ptr;
+    ptr = (char*)MEM_malloc(10);
+    for (int i = 0; i < 10; ++i) {
+        ptr[i] = 0xaa;
+    }
+    MEM_dump_memory();
+    MEM_free(ptr);
+    MEM_dump_memory();
+    
+    MEM_Storage storage = MEM_open_storage(0);
+
+
+    ptr = MEM_storage_malloc(storage, 17);
+    for (int i = 0; i < 17; ++i) {
+        ptr[i] = 0xee;
+    }
+    ptr = MEM_storage_malloc(storage, 10);
+    for (int i = 0; i < 10; ++i) {
+        ptr[i] = 0xef;
+    }
+
+    printf("-----------------------\n");
+    MEM_dump_memory();
+    MEM_dispose(storage);
+    MEM_dump_memory();
+    
+    
+    return (EXIT_SUCCESS);
+}
+
---
+++ memory/memory.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   mem.h
+ * Author: hiroaki
+ *
+ * Created on December 22, 2016, 10:04 AM
+ */
+
+#ifndef _MEMORY_H_
+#define _MEMORY_H_
+#include "MEM.h"
+
+typedef union Header_tag Header;
+
+struct MEM_Controller_tag {
+    Header *block_header;
+};
+
+
+
+#endif /* MEMORY_H */
+
