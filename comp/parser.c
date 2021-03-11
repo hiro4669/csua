36b7f1ade11acd18fc4ad50599d9855bfc5504b0
Hiroaki Fukuda <hiroaki@java2.jp>
2021-03-12 08:38:52 +0900

    add LP/RP

--- comp/parser.c
+++ comp/parser.c
@@ -10,13 +10,15 @@ extern int yylex();
 
 static int t_type;
 
+static int EFLG = OK;
+
 static void token_test() {    
 
     while (1) {
         t_type = yylex();
         switch (t_type) {
             case SEMICOLON: {
-                printf("SEEMICOLON(;)\n");
+                printf("SEMICOLON(;)\n");
                 break;
             }
             case INT_LITERAL: {
@@ -39,6 +41,14 @@ static void token_test() {
                 printf("DIV(/)\n");
                 break;
             }
+            case LP: {
+                printf("(\n");
+                break;
+            }
+            case RP: {
+                printf(")\n");
+                break;
+            }
             case EOF: {
                 printf("END OF FILE\n");
                 exit(1);
@@ -48,39 +58,78 @@ static void token_test() {
     }    
 }
 
-static int factor() {
+static void expression();
+
+static void factor() {
     printf("factor\n");
+
+    switch (t_type) {
+        case INT_LITERAL: {
+            t_type = yylex();
+            break;
+        }
+        case LP: {
+            t_type = yylex();
+            expression();
+            if (t_type == RP) {
+                t_type = yylex();
+            } else {
+                EFLG = ERR;
+            }
+            break;
+        }
+        default: {
+            printf("hoge?\n");
+            EFLG = ERR;
+            break;
+        }
+    }
+
+    /*
     if (t_type == INT_LITERAL) {        
         t_type = yylex();
-        return OK;
+        return;
     }
-    return ERR;
+    */
+    
 }
 
-static int term() {
+static void term() {
+    printf("term\n");
     factor();
-    return OK;
+    if (t_type == MUL | t_type == DIV) {
+        printf("* | /\n");
+        t_type = yylex();
+        factor();
+    }    
 }
 
-static int expression() {
+static void expression() {
+    printf("expression\n");
     term();
-    return OK;
+    if (t_type == ADD | t_type == DIV) {
+        printf("+ | -\n");
+        t_type = yylex();
+        term();
+    }
 }
 
 
-static int statement() {
+static void statement() {
+    printf("statement\n");
     expression();
     //t_type = yylex();
     if (t_type == SEMICOLON) {
-        printf("semicolon\n");
-        return OK;
+        //printf("semicolon\n");
+        return;
     } 
     t_type = yylex();
     if (t_type == EOF) {
         printf("end of input\n");
         exit(1);
     }
-    return ERR;
+    
+    EFLG = ERR;
 }
 
 
@@ -92,13 +141,15 @@ int main(void) {
         exit(1);
     }
     printf("Hello World\n");
-    t_type = yylex();
-    int r = statement();
-
-    if (r == OK) {
-        printf("parsing success\n");
-    } else {
-        printf("parsing error\n");
-    }
     //token_test();
+    
+    t_type = yylex();
+    statement();
+
+    if (EFLG == ERR) {
+        printf("parse error\n");
+    } else {
+        printf("parse success\n");
+    }
+    
 }
