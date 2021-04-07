290a21d141bfd9906e5a800d12fec0356f672d18
Hiroaki Fukuda <hiroaki@java2.jp>
2021-04-07 18:16:07 +0900

    add disasm

--- svm/svm.h
+++ svm/svm.h
@@ -50,4 +50,60 @@ typedef struct {
 } CS_Executable;
 
 
+typedef enum {
+    SVM_PUSH_INT = 1,
+    SVM_PUSH_DOUBLE,
+    SVM_PUSH_STACK_INT,
+    SVM_PUSH_STACK_DOUBLE,
+    SVM_POP_STACK_INT,
+    SVM_POP_STACK_DOUBLE,
+    SVM_PUSH_STATIC_INT,
+    SVM_PUSH_STATIC_DOUBLE,
+    SVM_POP_STATIC_INT,
+    SVM_POP_STATIC_DOUBLE,
+    SVM_ADD_INT,
+    SVM_ADD_DOUBLE,
+    SVM_SUB_INT,
+    SVM_SUB_DOUBLE,
+    SVM_MUL_INT,
+    SVM_MUL_DOUBLE,
+    SVM_DIV_INT,
+    SVM_DIV_DOUBLE,
+    SVM_MOD_INT,
+    SVM_MOD_DOUBLE,
+    SVM_MINUS_INT,
+    SVM_MINUS_DOUBLE,
+    SVM_INCREMENT,
+    SVM_DECREMENT,
+    SVM_CAST_INT_TO_DOUBLE,
+    SVM_CAST_DOUBLE_TO_INT,
+    SVM_EQ_INT,
+    SVM_EQ_DOUBLE,
+    SVM_NE_INT,
+    SVM_NE_DOUBLE,
+    SVM_GT_INT,
+    SVM_GT_DOUBLE,
+    SVM_GE_INT,
+    SVM_GE_DOUBLE,
+    SVM_LT_INT,
+    SVM_LT_DOUBLE,
+    SVM_LE_INT,
+    SVM_LE_DOUBLE,
+    SVM_LOGICAL_AND,
+    SVM_LOGICAL_OR,
+    SVM_LOGICAL_NOT,
+    SVM_POP,
+    SVM_PUSH_FUNCTION,
+    SVM_INVOKE,
+    SVM_RETURN
+} SVM_Opcode;
+
+typedef struct {
+    char    *opname;
+    char    *parameter;    
+    uint8_t s_size;
+} OpcodeInfo;
+
+extern OpcodeInfo svm_opcode_info[];
+
 #endif
